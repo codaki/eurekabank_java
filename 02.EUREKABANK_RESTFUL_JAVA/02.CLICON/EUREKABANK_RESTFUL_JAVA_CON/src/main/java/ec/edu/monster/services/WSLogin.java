537cf272195b49d7a0a0085f0a0652bee6a3c7fb
Christopher Iza <cdiza5@espe.edu.ec>
2024-12-08 14:35:18 -0500

    Consola restful

---
+++ 02.EUREKABANK_RESTFUL_JAVA/02.CLICON/EUREKABANK_RESTFUL_JAVA_CON/src/main/java/ec/edu/monster/services/WSLogin.java
@@ -0,0 +1,43 @@
+
+package ec.edu.monster.services;
+
+import com.fasterxml.jackson.core.util.JacksonFeature;
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+/**
+ * Jersey REST client generated for REST resource:LoginResource [login]<br>
+ * USAGE:
+ * <pre>
+ *        WSLogin client = new WSLogin();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author danie
+ */
+public class WSLogin {
+
+    private WebTarget webTarget;
+    private Client client;
+    private static final String BASE_URI = "http://localhost:8080/EUREKABANK_RESTFUL_JAVA/resources";
+
+    public WSLogin() {
+       client = ClientBuilder.newBuilder()
+                .register(JacksonFeature.class) // Registrar Jackson para JSON
+                .build();
+        webTarget = client.target(BASE_URI).path("login");
+    }
+
+    public <T> T authenticate(Object requestEntity, Class<T> responseType) throws ClientErrorException {
+        return webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON), responseType);
+    }
+
+    public void close() {
+        client.close();
+    }
+    
+}
